564ce898e5232a415196ff89edd45026213a86d9
Joshua Soberg <j15soberg@gmail.com>
2022-08-28 12:25:25 -0400

    Add LootCalculator and initial rounding implementation for calculating a loot split

---
+++ domain/src/main/kotlin/com/soberg/lootsplit/domain/calculate/RoundingLootCalculator.kt
@@ -0,0 +1,40 @@
+package com.soberg.lootsplit.domain.calculate
+
+import com.soberg.lootsplit.domain.Coin
+import com.soberg.lootsplit.domain.LootSummation
+
+/** [LootCalculator] implementation that will round to the specified [Coin] in [roundTo] after splitting. */
+class RoundingLootCalculator(
+    private val roundTo: Coin = Coin.Gold,
+) : LootCalculator {
+
+    override fun calculate(
+        numPlayers: Int,
+        loot: LootSummation,
+    ): LootCalculator.Result {
+        val lootBuilder = LootSummation.Builder()
+
+        // First calculate our roundTo coin.
+        val baseCoinAmount = loot.splitToBase()
+        val roundCoinAmount = (baseCoinAmount / roundTo.baseCopperValue)
+        lootBuilder.add(roundCoinAmount / numPlayers, roundTo)
+        var leftoverCopper = (baseCoinAmount % roundTo.baseCopperValue) +
+                roundTo.toBaseCoinAmount(roundCoinAmount % numPlayers)
+
+        // Now, iterate down from the rounding coin until we have no leftover.
+        var currentCoin: Coin? = roundTo.nextLowestValueCoin
+        while (currentCoin != null && leftoverCopper != 0L) {
+            val currentCoinAmount = (leftoverCopper / currentCoin.baseCopperValue)
+            lootBuilder.add(currentCoinAmount / numPlayers, currentCoin)
+
+            leftoverCopper = (leftoverCopper % currentCoin.baseCopperValue) +
+                    currentCoin.toBaseCoinAmount(currentCoinAmount % numPlayers)
+            currentCoin = currentCoin.nextLowestValueCoin
+        }
+        return LootCalculator.Result(
+            numPlayers = numPlayers,
+            lootPerPlayer = lootBuilder.build(),
+            leftoverLoot = LootSummation.build { add(leftoverCopper, Coin.Copper) },
+        )
+    }
+}
---
+++ domain/src/test/kotlin/com/soberg/lootsplit/domain/calculate/RoundingLootCalculatorTest.kt
@@ -0,0 +1,84 @@
+package com.soberg.lootsplit.domain.calculate
+
+import com.google.common.truth.Truth.assertThat
+import com.soberg.lootsplit.domain.Coin
+import com.soberg.lootsplit.domain.LootSummation
+import org.junit.jupiter.api.Test
+
+internal class RoundingLootCalculatorTest {
+
+    @Test
+    fun `round with expected coin steps, starting with roundTo and iterating down, with expected leftover amount`() {
+        val calculator = RoundingLootCalculator(roundTo = Coin.Gold)
+        // (5g + 9e + 49s + 3c) = (14g + 4s + 3c)
+        val loot = LootSummation.build {
+            add(5, Coin.Gold)
+            add(9, Coin.Electrum)
+            add(49, Coin.Silver)
+            add(3, Coin.Copper)
+        }
+        val result = calculator.calculate(numPlayers = 4, loot)
+        // When we split, starting at Gold:
+        // 1. ((1443c / 100c/g) = 14g 43c) / 4 = 3g (243c leftover)
+        // 2. ((243c / 50c/e) = 4e 43c) / 4 = 1e (43c leftover)
+        // 3. ((43c / 10c/s) = 4s 3c) / 4 = 1s (3c leftover)
+        // 4. ((3c / 1c/c) = 3c) / 4 = 0c (3c leftover)
+        assertThat(result.lootPerPlayer[Coin.Platinum]).isNull()
+        assertThat(result.lootPerPlayer[Coin.Gold]).isEqualTo(3L)
+        assertThat(result.lootPerPlayer[Coin.Electrum]).isEqualTo(1L)
+        assertThat(result.lootPerPlayer[Coin.Silver]).isEqualTo(1L)
+        assertThat(result.lootPerPlayer[Coin.Copper]).isNull()
+
+        assertThat(result.leftoverLoot).isNotNull()
+        assertThat(result.leftoverLoot!!.keys).containsExactly(Coin.Copper)
+        assertThat(result.leftoverLoot!![Coin.Copper]).isEqualTo(3L)
+    }
+
+    @Test
+    fun `round as expected when roundTo is base coin`() {
+        val calculator = RoundingLootCalculator(roundTo = Coin.Copper)
+        val loot = LootSummation.build {
+            add(5, Coin.Gold)
+            add(9, Coin.Electrum)
+            add(49, Coin.Silver)
+            add(3, Coin.Copper)
+        }
+        val result = calculator.calculate(numPlayers = 4, loot)
+        assertThat(result.lootPerPlayer.keys).containsExactly(Coin.Copper)
+        assertThat(result.lootPerPlayer[Coin.Copper]).isEqualTo(360L)
+
+        assertThat(result.leftoverLoot).isNotNull()
+        assertThat(result.leftoverLoot!!.keys).containsExactly(Coin.Copper)
+        assertThat(result.leftoverLoot!![Coin.Copper]).isEqualTo(3L)
+    }
+
+    @Test
+    fun `round to expected coin even when higher value coins can be split evenly`() {
+        val calculator = RoundingLootCalculator(roundTo = Coin.Gold)
+        val loot = LootSummation.build {
+            add(10, Coin.Platinum)
+            add(10, Coin.Gold)
+            add(20, Coin.Electrum)
+            add(100, Coin.Silver)
+            add(1000, Coin.Copper)
+        }
+        val result = calculator.calculate(numPlayers = 10, loot)
+        // (1p + 1g + 2e + 10s + 100c) per player expected, which rounds to 14g
+        assertThat(result.lootPerPlayer.keys).containsExactly(Coin.Gold)
+        assertThat(result.lootPerPlayer[Coin.Gold]).isEqualTo(14L)
+    }
+
+    @Test
+    fun `calculate when loot can't be split`() {
+        val calculator = RoundingLootCalculator(roundTo = Coin.Gold)
+        val loot = LootSummation.build {
+            add(11, Coin.Copper)
+        }
+        val result = calculator.calculate(numPlayers = 12, loot)
+        assertThat(result.lootPerPlayer).isEmpty()
+
+        assertThat(result.leftoverLoot).isNotNull()
+        assertThat(result.leftoverLoot!!.keys).containsExactly(Coin.Copper)
+        assertThat(result.leftoverLoot!![Coin.Copper]).isEqualTo(11L)
+    }
+}
